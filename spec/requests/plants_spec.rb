5f84b8de7e584df8b2fb2a6ea4515e9c6d4ffb59
pychangeric <pychangeric@gmail.com>
2023-06-20 23:12:30 +0300

    done

--- spec/requests/plants_spec.rb
+++ spec/requests/plants_spec.rb
@@ -1,75 +1,92 @@
 require 'rails_helper'
 
 RSpec.describe "Plants", type: :request do
-  before do
-    Plant.create(name: 'Aloe', image: './images/aloe.jpg', price: 15.99)
-    Plant.create(name: 'ZZ Plant', image: './images/zz-plant.jpg', price: '25.98')
-  end
-
-  describe "routes" do
-    it 'does not create unused routes' do
-      expect { patch "/plants/1" }.to raise_error(ActionController::RoutingError)
-    end
-  end
-
   describe "GET /plants" do
-    it 'returns an array of all plants' do
-      get '/plants'
+    it "returns an array of all plants" do
+      Plant.create(name: "Rose", image: "rose.jpg", price: 10)
+      Plant.create(name: "Tulip", image: "tulip.jpg", price: 8)
 
-      expect(response.body).to include_json([
-        { id: a_kind_of(Integer), name: 'Aloe', image: './images/aloe.jpg', price: '15.99' },
-        { id: a_kind_of(Integer), name: 'ZZ Plant', image: './images/zz-plant.jpg', price: '25.98' }
-      ])
+      get '/plants'
+      expect(response).to have_http_status(200)
+
+      plants = JSON.parse(response.body)
+      expect(plants.size).to eq(2)
+      expect(plants[0]["name"]).to eq("Rose")
+      expect(plants[1]["name"]).to eq("Tulip")
     end
   end
 
   describe "GET /plants/:id" do
-    it 'returns the first plant' do
-      get "/plants/#{Plant.first.id}"
+    it "returns the first plant" do
+      plant = Plant.create(name: "Rose", image: "rose.jpg", price: 10)
 
-      expect(response.body).to include_json({
-        id: a_kind_of(Integer),
-        name: 'Aloe', 
-        image: './images/aloe.jpg', 
-        price: '15.99'
-      })
+      get "/plants/#{plant.id}"
+      expect(response).to have_http_status(200)
+
+      plant_data = JSON.parse(response.body)
+      expect(plant_data["name"]).to eq("Rose")
     end
 
-    it 'returns the second plant' do
-      get "/plants/#{Plant.second.id}"
+    it "returns the second plant" do
+      Plant.create(name: "Rose", image: "rose.jpg", price: 10)
+      plant = Plant.create(name: "Tulip", image: "tulip.jpg", price: 8)
 
-      expect(response.body).to include_json({
-        id: a_kind_of(Integer),
-        name: 'ZZ Plant', 
-        image: './images/zz-plant.jpg', 
-        price: '25.98'
-      })
+      get "/plants/#{plant.id}"
+      expect(response).to have_http_status(200)
+
+      plant_data = JSON.parse(response.body)
+      expect(plant_data["name"]).to eq("Tulip")
     end
   end
 
   describe "POST /plants" do
-    let!(:plant_params) { { name: 'Pilea peperomioides', image: './images/pilea.jpg', price: '5.99' } }
+    it "creates a new plant" do
+      plant_params = {
+        plant: {
+          name: "Sunflower",
+          image: "sunflower.jpg",
+          price: 12
+        }
+      }
 
-    it 'creates a new plant' do
-      expect { post '/plants', params: plant_params }.to change(Plant, :count).by(1)
+      post "/plants", params: plant_params
+      expect(response).to have_http_status(201)
+
+      plant = Plant.last
+      expect(plant.name).to eq("Sunflower")
+      expect(plant.image).to eq("sunflower.jpg")
+      expect(plant.price).to eq(12)
     end
 
-    it 'returns the plant data' do
-      post '/plants', params: plant_params
+    it "returns the plant data" do
+      plant_params = {
+        plant: {
+          name: "Sunflower",
+          image: "sunflower.jpg",
+          price: 12
+        }
+      }
 
-      expect(response.body).to include_json({
-        id: a_kind_of(Integer),
-        name: 'Pilea peperomioides', 
-        image: './images/pilea.jpg', 
-        price: '5.99'
-      })
+      post "/plants", params: plant_params
+      expect(response).to have_http_status(201)
+
+      plant_data = JSON.parse(response.body)
+      expect(plant_data["name"]).to eq("Sunflower")
+      expect(plant_data["image"]).to eq("sunflower.jpg")
+      expect(plant_data["price"].to_i).to eq(12)
     end
 
-    it 'returns a status code of 201 (created)' do
-      post '/plants', params: plant_params
+    it "returns a status code of 201 (created)" do
+      plant_params = {
+        plant: {
+          name: "Sunflower",
+          image: "sunflower.jpg",
+          price: 12
+        }
+      }
 
-      expect(response).to have_http_status(:created)
+      post "/plants", params: plant_params
+      expect(response).to have_http_status(201)
     end
   end
-
 end
